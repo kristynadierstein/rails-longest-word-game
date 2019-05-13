b18afc6f672daf19f6f240ce9ad85d995b9f066f
Kristyna Dierstein <kristynahorankova@gmail.com>
2019-05-13 16:54:29 -0400

    proposing letters, not working for last method - check if array contains

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,20 @@
 class GamesController < ApplicationController
   def new
-      charset = Array('A'..'Z') + Array('a'..'z')
-      @letters = Array.new(10) { charset.sample }
-      raise
+      charset = Array('A'..'Z')
+      @letters = Array.new(10) { charset.sample }.join(" ")
   end
 
   def score
+    @word = params['new']
+    @new_letters = @word.split("")
+    @original_letters = params["original_letters"]
+    @original_split = @original_letters.split
+    check_if_in_array(new_letters, original_split)
   end
+
+  def check_if_in_array()
+
+  end
+
+
 end
